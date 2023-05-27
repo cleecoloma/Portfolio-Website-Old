5169fce9d629e0beef048e4fea0e835dcd750f13
Chester Lee Coloma <cleecoloma@gmail.com>
2023-05-27 16:43:06 -0700

    added page names

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,20 +4,18 @@ import Navbar from "react-bootstrap/Navbar";
 
 function Header() {
   return (
-    <>
-      <Navbar bg="light" variant="light">
-        <Container>
-          <Navbar.Brand href="#home">Chester</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#projects">Projects</Nav.Link>
-            <Nav.Link href="#about">About</Nav.Link>
-            <Nav.Link href="#blog">Blog</Nav.Link>
-            <Nav.Link href="#contact">Contact</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
-    </>
+    <Navbar bg="light" variant="light">
+      <Container>
+        <Navbar.Brand href="#home">Chester</Navbar.Brand>
+        <Nav className="me-0">
+          <Nav.Link href="#home">Home</Nav.Link>
+          <Nav.Link href="#projects">Projects</Nav.Link>
+          <Nav.Link href="#blog">Blog</Nav.Link>
+          <Nav.Link href="#about">About</Nav.Link>
+          <Nav.Link href="#contact">Contact</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
   );
 }
 
