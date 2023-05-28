c224dcfcbbae887171c237976f697de3f93d23ed
Chester Lee Coloma <cleecoloma@gmail.com>
2023-05-27 19:28:25 -0700

    added linkedin, github, and resume buttons

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,11 +8,21 @@ function Header() {
       <Container>
         <Navbar.Brand href="#home">Chester</Navbar.Brand>
         <Nav className="me-0">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="#projects">Projects</Nav.Link>
-          <Nav.Link href="#blog">Blog</Nav.Link>
-          <Nav.Link href="#about">About</Nav.Link>
-          <Nav.Link href="#contact">Contact</Nav.Link>
+          <Nav.Link href="/home">
+            Home
+          </Nav.Link>
+          <Nav.Link href="/projects">
+            Projects
+          </Nav.Link>
+          <Nav.Link href="/blog">
+            Blog
+          </Nav.Link>
+          <Nav.Link href="/about">
+            About
+          </Nav.Link>
+          <Nav.Link href="/contact">
+            Contact
+          </Nav.Link>
         </Nav>
       </Container>
     </Navbar>
--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,23 +2,31 @@ import Button from "react-bootstrap/Button";
 import LinkedInIcon from "@mui/icons-material/LinkedIn";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import PictureAsPdfIcon from "@mui/icons-material/PictureAsPdf";
+import { Container } from "react-bootstrap";
 
 function Home() {
   return (
-    <div>
+    <Container>
       <h1>Hi, I'm Chester!</h1>
-      <h2>A front-end web developer.</h2>
+      <h2>
+        <p>
+          A <span className="text-muted">mechanical engineer</span> turned{" "}
+          <span style={{ color: "blue" }}>front-end web developer</span>.
+        </p>
+      </h2>
       <Button variant="outline-primary">
         <LinkedInIcon />
         LinkedIn
       </Button>{" "}
       <Button variant="outline-dark">
         <GitHubIcon />
-        GitHub</Button>{" "}
+        GitHub
+      </Button>{" "}
       <Button variant="outline-success">
         <PictureAsPdfIcon />
-        Resume</Button>{" "}
-    </div>
+        Resume
+      </Button>{" "}
+    </Container>
   );
 }
 
