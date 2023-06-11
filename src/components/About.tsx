a7b3653b1956c93bbe71e0eb6a7a93dff0ee08ec
Chester Lee Coloma <cleecoloma@gmail.com>
2023-06-11 16:05:21 -0700

    added placeholder for about me paragraphs

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -5,7 +5,28 @@ function About() {
   return (
     <Container>
       <h1>About</h1>
-      <p>Hi! I'm Chester Lee Coloma. I'm currently a mechanical engineer working at Puget Sound Naval Shipyard. I have been a mechanical engineer for about 9 years now. I'm transitioning on becoming a software developer. I will be going through the Code Fellows software developement bootcamp this July 2023.</p>
+      <p>
+        Hi! I'm Chester Lee Coloma. I'm currently a mechanical engineer working
+        at Puget Sound Naval Shipyard. I have been a mechanical engineer for
+        about 9 years now. I'm transitioning on becoming a software developer. I
+        will be going through the Code Fellows software developement bootcamp
+        this July 2023.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Duis convallis
+        convallis tellus id interdum velit laoreet id. Quis lectus nulla at
+        volutpat diam ut. Sit amet aliquam id diam. In dictum non consectetur a
+        erat nam. Dignissim diam quis enim lobortis scelerisque fermentum.
+        Mauris augue neque gravida in fermentum et sollicitudin ac. Eu facilisis
+        sed odio morbi quis.
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Justo nec ultrices
+        dui sapien eget mi proin sed libero. Habitant morbi tristique senectus
+        et netus et malesuada fames.
+      </p>
       <hr />
     </Container>
   );
