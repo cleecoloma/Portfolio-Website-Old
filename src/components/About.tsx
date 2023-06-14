45e508448c14c913a1337a314bd4f824f275aaf1
Chester Lee Coloma <cleecoloma@gmail.com>
2023-06-13 21:21:32 -0700

    added more about me write up

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -9,15 +9,16 @@ function About() {
         Hi! I'm Chester Lee Coloma. I'm currently a mechanical engineer working
         at Puget Sound Naval Shipyard. I have been a mechanical engineer for
         about 9 years now. I'm transitioning on becoming a software developer. I
-        will be going through the Code Fellows software developement bootcamp
+        will be going through the Code Fellows software development bootcamp
         this July 2023.
       </p>
       <p>
-        Background - I first learned about coding when I was assigned to be a webmaster for one of my Mechanical Engineering class. At that time, I only learned about HTML and CSS and updated the website using them.
+        *Need to work on it*
+        *Background - I first learned about coding when I was assigned to be a webmaster for one of my Mechanical Engineering class. At that time, I only learned about HTML and CSS and updated the website using them.
 
-        Transition - During the past year, I have been coding on and off after work and sometimes during the weekends. I have learning from YouTube videos and Udemy courses. At this time, I found myself thinking that I could make it my career. 
+        *Transition - During the past year, I have been coding on and off after work and sometimes during the weekends. I have learning from YouTube videos and Udemy courses. At this time, I found myself thinking that I could make it my career. 
 
-        Current - As an investment in myself, I have decided to attend a coding bootcamp. I have done a lot of research about in-person and remote coding bootcamps.
+        *Current - As an investment in myself, I have decided to attend a coding bootcamp. I have done a lot of research about in-person and remote coding bootcamps. Being able to attend an in-person bootcamp fits my learning style. There were only two in-person coding bootcamps close to me. In the end, I went with Code Fellows. It is Seattle's first code school.
       </p>
       <p>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
