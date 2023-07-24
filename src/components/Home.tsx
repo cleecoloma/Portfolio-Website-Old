b1d2bd5d339ff8dfeece1d9d4246b4101c740206
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-23 21:45:43 -0700

    added styling branch

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -2,13 +2,17 @@ import { Container } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 
 function Home() {
+  const myStyle = {
+    backgroundColor: "gray",
+    height: "400px"
+  }
   return (
-    <Container>
+    <Container style={myStyle}>
       <h1>Hi, I'm Chester!</h1>
       <h2>
         <p>
           A <span className="text-muted">mechanical engineer</span> turned{" "}
-          <span style={{ color: "blue" }}>front-end web developer</span>.
+          <span style={{ color: "blue" }}>software developer</span>.
         </p>
       </h2>
       <Button variant="primary">See My Projects</Button>{" "}
