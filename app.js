060945b020b9088fa842bab91c35d4d82ad84b59
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-30 16:07:02 -0700

    added 2 project placeholders

---
+++ app.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const projects = [];
+
+function Projects(
+  name,
+  imageSource,
+  description,
+  tech,
+  codeLink,
+  deployedLink
+) {
+  this.name = name;
+  this.imageSource = imageSource;
+  this.description = description;
+  this.tech = tech;
+  this.codeLink = codeLink;
+  this.deployedLink = deployedLink;
+  projects.push(this);
+}
+
+const PROJECTS = document.getElementById('project-cards');
+// const PROJECT_CARDS = document.getElementById('cards');
+
+function displayProjects(projects) {
+  for (let i = 0; i < projects.length; i++) {
+    let cardElement = document.createElement('div');
+    let imgElement = document.createElement('img');
+    let divElement = document.createElement('div');
+    let h4Element = document.createElement('h4');
+    let pElement = document.createElement('p');
+    cardElement.setAttribute('id', 'cards');
+    imgElement.setAttribute('src', projects[i].imageSource);
+    cardElement.appendChild(imgElement);
+    divElement.setAttribute('class', 'card-description');
+    h4Element.textContent = projects[i].name;
+    pElement.textContent = projects[i].description;
+    divElement.appendChild(h4Element);
+    divElement.appendChild(pElement);
+    cardElement.appendChild(imgElement);
+    cardElement.appendChild(divElement);
+    PROJECTS.appendChild(cardElement);
+  }
+}
+
+new Projects(
+  'Landscaping Company',
+  'img/landscaping-company.png',
+  'This is a landing page for a lawn company. A static landing page website that is responsive and works well with multiple screen sizes, including desktops and mobile phones, is designed to provide a consistent and optimized user experience regardless of the device being used.'
+);
+
+new Projects(
+  'Landscaping Company 2',
+  'img/landscaping-company.png',
+  'This is a landing page for a lawn company. A static landing page website that is responsive and works well with multiple screen sizes, including desktops and mobile phones, is designed to provide a consistent and optimized user experience regardless of the device being used.'
+);
+
+new Projects(
+  'Landscaping Company 3',
+  'img/landscaping-company.png',
+  'This is a landing page for a lawn company. A static landing page website that is responsive and works well with multiple screen sizes, including desktops and mobile phones, is designed to provide a consistent and optimized user experience regardless of the device being used.'
+);
+
+displayProjects(projects);
+console.log(projects);
