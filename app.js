af6d852d386c4e5cbcbd6f85dc8e47814bd1bd0a
Chester Lee Coloma <cleecoloma@gmail.com>
2023-07-31 07:53:40 -0700

    created createAnElement function

--- app.js
+++ app.js
@@ -20,17 +20,28 @@ function Projects(
 }
 
 const PROJECTS = document.getElementById('project-cards');
-// const PROJECT_CARDS = document.getElementById('cards');
 
+//
+const listOfElements = ['div', 'img', 'div', 'h4', 'p']
+
+// creates elements
+function createAnElement(elementName, element, id, attribute) {
+  elementName = document.createElement(element);
+  elementName.setAttribute(id, attribute)
+  return elementName;
+}
+
+// display each project from projects array
 function displayProjects(projects) {
   for (let i = 0; i < projects.length; i++) {
+    let imgElement = createAnElement('imgElement', 'img', 'id', projects[i].imageSource);
     let cardElement = document.createElement('div');
-    let imgElement = document.createElement('img');
+    // let imgElement = document.createElement('img');
     let divElement = document.createElement('div');
     let h4Element = document.createElement('h4');
     let pElement = document.createElement('p');
     cardElement.setAttribute('id', 'cards');
-    imgElement.setAttribute('src', projects[i].imageSource);
+    // imgElement.setAttribute('src', projects[i].imageSource);
     cardElement.appendChild(imgElement);
     divElement.setAttribute('class', 'card-description');
     h4Element.textContent = projects[i].name;
